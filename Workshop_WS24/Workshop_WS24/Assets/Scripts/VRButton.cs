8114bb5938345013f6716da4c793636d2907bc3a
Simon Saurer <s2310238062@fhooe.at>
2025-06-04 17:17:36 +0200

    coffee grinder and machine fixed, 3d button started

---
+++ Workshop_WS24/Workshop_WS24/Assets/Scripts/VRButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
+
+public class VRButton : MonoBehaviour
+{
+    public CoffeeMachine coffeeMachine;
+
+    public void TriggerBrew()
+    {
+        coffeeMachine.StartBrewing();
+        PressEffect();
+    }
+
+    public void PressButton()
+    {
+        Debug.Log("Button pressed!");
+        TriggerBrew();
+        
+        // Hier kommt deine Logik hin (z. B. Kaffee starten)
+    }
+
+    private IEnumerator PressEffect()
+    {
+        transform.localScale *= 0.9f;
+        yield return new WaitForSeconds(0.1f);
+        transform.localScale /= 0.9f;
+    }
+
+}
